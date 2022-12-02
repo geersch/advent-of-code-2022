f784b0bf498ce270971582b88545d963e89882d8
Christophe Geers <geersch@gmail.com>
2022-12-02 19:46:14 +0100

    day 1

---
+++ src/day1/index.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const data = readFileSync(join(__dirname, 'input.txt'), {
+  encoding: 'utf8',
+  flag: 'r',
+});
+
+const items: string[] = data.split(/\r?\n/);
+
+let total = 0;
+const totals: number[] = [];
+items.forEach((item: string) => {
+  const value = parseInt(item, 10);
+  if (isNaN(value)) {
+    totals.push(total);
+    total = 0;
+  } else {
+    total += value;
+  }
+});
+
+totals.sort((a, b) => b - a);
+
+function getTopX(count: number): number {
+  let total = 0;
+  for (let i = 0; i < count; i++) {
+    total += totals[i];
+  }
+  return total;
+}
+
+console.log(`The most calories caried by an elf: ${getTopX(1)}.`);
+console.log(`The top three Elves are carying ${getTopX(3)} calories.`);
