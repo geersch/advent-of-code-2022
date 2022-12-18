bc13d60f18102a05057c0aebb06a255e84ad669f
Christophe Geers <geersch@gmail.com>
2022-12-18 11:19:36 +0100

    refactor(day16): increased performance from +/-10s to +/-200ms

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -42,136 +42,53 @@ for (const valve of Object.values(valves)) {
   calcDistances(valves, valve);
 }
 
-type Node = {
+function addFlow(valves: Graph, openValves: string[]): number {
+  let sum = 0;
+  openValves.forEach((valve) => (sum += valves[valve].flowRate));
+  return sum;
+}
+
+type Path = {
   name: string;
-  time: number;
+  timeLeft: number;
   pressure: number;
-  openValves: Record<string, number>; // Remember what minute the valves were opened
+  openValves: string[];
+  toVisit: string[];
 };
 
 let pressureReleased = 0;
 
-const queue: Node[] = [
+const paths: Path[] = [
   {
     name: 'AA',
-    time: 30,
+    timeLeft: 30,
     pressure: 0,
-    openValves: {},
+    openValves: [],
+    toVisit: Object.values(valves)
+      .filter((valve) => valve.flowRate > 0)
+      .map((valve) => valve.name),
   },
 ];
 
-function addFlow(valves: Graph, openValves: Record<string, number>) {
-  let sum = 0;
-  for (const key in openValves) {
-    sum += valves[key].flowRate;
-  }
-  return sum;
-}
-
-while (queue.length > 0) {
-  const current = queue.shift()!;
-
-  // What are the non-opened valves, with a non-zero flow rate I can still open for this node?
-  const options = Object.values(valves).filter((valve) => valve.flowRate > 0 && !current.openValves[valve.name]);
-  if (options.length === 0) {
-    const pressure = current.pressure + current.time * addFlow(valves, current.openValves);
-    if (pressure > pressureReleased) {
-      pressureReleased = pressure;
-    }
-    continue;
-  }
-
-  for (const valve of options) {
-    const steps = valves[current.name].distances[valve.name] + 1; // +1 for opening the valve
-    // Is there still time left?
-    if (current.time - steps <= 0) {
-      const pressure = current.pressure + current.time * addFlow(valves, current.openValves);
+for (let i = 0; i < paths.length; i++) {
+  const current = paths[i];
+  for (const valve of current.toVisit) {
+    const steps = valves[current.name].distances[valve] + 1; // +1 for opening the valve
+    if (current.timeLeft - steps <= 0) {
+      const pressure = current.pressure + current.timeLeft * addFlow(valves, current.openValves);
       if (pressure > pressureReleased) {
         pressureReleased = pressure;
       }
     } else {
-      queue.push({
-        name: valve.name,
-        time: current.time - steps,
+      paths.push({
+        name: valve,
+        timeLeft: current.timeLeft - steps,
         pressure: current.pressure + steps * addFlow(valves, current.openValves),
-        openValves: { ...current.openValves, [valve.name]: current.time - steps },
+        openValves: [...current.openValves, valve],
+        toVisit: current.toVisit.filter((v) => v !== valve),
       });
     }
   }
 }
 
 console.log(`The most pressure you can release is ${pressureReleased}.`);
-
-// A naive attempt which happened to work for the sample, sigh.
-
-// function getNeighbours(valves: Valve[], valve: Valve): Valve[] {
-//   return valve.neighbours.map((tunnel) => valves.find((v) => v.name === tunnel)!);
-// }
-
-// function plotRoute(valves: Valve[], start: Valve, destination: Valve): number {
-//   const queue: { valve: Valve; cost: number }[] = [{ valve: start, cost: 0 }];
-//   const visited = new Set<string>([`${start.name}`]);
-//   while (queue.length) {
-//     const { cost, valve } = queue.shift()!;
-//     if (valve.name === destination.name) {
-//       return cost;
-//     }
-
-//     getNeighbours(valves, valve).forEach((neighbour) => {
-//       if (!visited.has(neighbour.name)) {
-//         visited.add(neighbour.name);
-//         queue.push({ valve: neighbour, cost: cost + 1 });
-//         queue.sort((a, b) => a.cost - b.cost);
-//       }
-//     });
-//   }
-
-//   return -1; // no route
-// }
-
-// function findNextDestination(valves: Valve[], start: Valve): { valve: Valve; cost: number } | undefined {
-//   //   console.log(`You are currently at valve ${start.name}.`);
-//   let costs: { valve: Valve; cost: number; gain: number }[] = [];
-//   for (const valve of valves.filter((v) => v.name !== start.name && !v.open && v.flowRate > 0)) {
-//     const cost = plotRoute(valves, start, valve);
-//     const gain = Math.ceil(valve.flowRate / cost);
-//     costs.push({ valve, cost, gain });
-//   }
-//   costs.sort((a, b) => b.gain - a.gain);
-//   costs = costs.filter((c) => c.gain === costs[0].gain).sort((a, b) => a.cost - b.cost);
-
-//   return costs.length ? { valve: costs[0].valve, cost: costs[0].cost } : undefined;
-// }
-
-// let start = valves.find((valve) => valve.name === 'AA')!;
-
-// let pressureReleased = 0;
-
-// let openValves: Valve[] = [];
-// let minute = 1;
-// let destination: { valve: Valve; cost: number } | undefined;
-// while (minute <= 30) {
-//   console.log(`== Minute ${minute} ==`);
-
-//   openValves.forEach((v) => {
-//     pressureReleased += v.flowRate;
-//   });
-
-//   if (!destination || destination.valve.open) {
-//     destination = findNextDestination(valves, start);
-//   }
-
-//   if (destination) {
-//     if (destination.cost > 0) {
-//       console.log(`You move towards valve ${destination.valve.name}.`);
-//     } else {
-//       console.log(`You open valve ${destination.valve.name}.`);
-//       destination.valve.open = true;
-//       openValves.push(destination.valve);
-//       start = destination.valve;
-//     }
-//     destination.cost -= 1;
-//   }
-
-//   minute++;
-// }
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function getProcessingTime(time: [number, number]) {
+  return `${(time[0] * 1000 + time[1] / 1e6).toFixed(2)}ms`;
+}
