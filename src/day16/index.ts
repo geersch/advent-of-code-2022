47b22807eb87621cf4185ae442cd66a6e3e8b429
Christophe Geers <geersch@gmail.com>
2022-12-18 21:32:40 +0100

    day 16 - part 2

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -1,5 +1,9 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import { hrtime } from 'process';
+import { getProcessingTime } from '../utils';
+
+const start = hrtime();
 
 type Valve = { name: string; flowRate: number; neighbours: string[]; distances: Record<string, number> };
 type Graph = Record<string, Valve>;
@@ -48,6 +52,8 @@ function addFlow(valves: Graph, openValves: string[]): number {
   return sum;
 }
 
+type Solution = { pressure: number; visited: string[] };
+
 type Path = {
   name: string;
   timeLeft: number;
@@ -56,39 +62,80 @@ type Path = {
   toVisit: string[];
 };
 
-let pressureReleased = 0;
-
-const paths: Path[] = [
-  {
-    name: 'AA',
-    timeLeft: 30,
-    pressure: 0,
-    openValves: [],
-    toVisit: Object.values(valves)
-      .filter((valve) => valve.flowRate > 0)
-      .map((valve) => valve.name),
-  },
-];
-
-for (let i = 0; i < paths.length; i++) {
-  const current = paths[i];
-  for (const valve of current.toVisit) {
-    const steps = valves[current.name].distances[valve] + 1; // +1 for opening the valve
-    if (current.timeLeft - steps <= 0) {
-      const pressure = current.pressure + current.timeLeft * addFlow(valves, current.openValves);
-      if (pressure > pressureReleased) {
-        pressureReleased = pressure;
+function solve(valves: Graph, start: Valve, timeLeft: number): Solution[] {
+  const solutions: Solution[] = [];
+  const paths: Path[] = [
+    {
+      name: start.name,
+      timeLeft: timeLeft,
+      pressure: 0,
+      openValves: [],
+      toVisit: Object.values(valves)
+        .filter((valve) => valve.flowRate > 0)
+        .map((valve) => valve.name),
+    },
+  ];
+  for (let i = 0; i < paths.length; i++) {
+    const current = paths[i];
+    for (const valve of current.toVisit) {
+      const steps = valves[current.name].distances[valve] + 1; // +1 for opening the valve
+      if (current.timeLeft - steps <= 0) {
+        solutions.push({
+          pressure: current.pressure + current.timeLeft * addFlow(valves, current.openValves),
+          visited: current.openValves,
+        });
+      } else {
+        paths.push({
+          name: valve,
+          timeLeft: current.timeLeft - steps,
+          pressure: current.pressure + steps * addFlow(valves, current.openValves),
+          openValves: [...current.openValves, valve],
+          toVisit: current.toVisit.filter((v) => v !== valve),
+        });
       }
-    } else {
-      paths.push({
-        name: valve,
-        timeLeft: current.timeLeft - steps,
-        pressure: current.pressure + steps * addFlow(valves, current.openValves),
-        openValves: [...current.openValves, valve],
-        toVisit: current.toVisit.filter((v) => v !== valve),
-      });
+    }
+  }
+
+  return solutions.sort((a, b) => b.pressure - a.pressure);
+}
+
+let { 0: solution1 } = solve(valves, valves['AA'], 30);
+console.log(`The most pressure you can release in 30 minutes is ${solution1.pressure}.`);
+
+// Part 2
+// - Calculate all the solutions for 26 minutes.
+// - Iterate over all the solutions and compare them
+//   against all other solutions of which the visited valves
+//   have no intersection.
+// - It their combined released pressure is higher than the previous
+//   max store it.
+// - Abort both loops as soon as possible.
+
+const solutions = solve(valves, valves['AA'], 26);
+
+let solution2 = 0;
+for (let i = 0; i < solutions.length; i++) {
+  const current = solutions[i];
+  // Stop this iteration as soon as the double of the current pressure can no longer exceed the maximum.
+  if (current.pressure * 2 < solution2) {
+    break;
+  }
+
+  for (let j = 1; j < solutions.length; j++) {
+    const next = solutions[j];
+    // Stop this iteration as soon as we cannot exceed the maximum.
+    if (current.pressure + next.pressure < solution2) {
+      break;
+    }
+    if (
+      current.pressure + next.pressure > solution2 &&
+      current.visited.every((valve) => !next.visited.includes(valve))
+    ) {
+      solution2 = current.pressure + next.pressure;
     }
   }
 }
 
-console.log(`The most pressure you can release is ${pressureReleased}.`);
+console.log(`The most pressure you can release in 26 minutes with the help of an elephant is ${solution2}.`);
+
+console.log(getProcessingTime(hrtime(start)));
