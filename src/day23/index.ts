47663cd1f9e7d21abb37a110e98bd5c16dc3e6b2
Christophe Geers <geersch@gmail.com>
2022-12-26 22:08:35 +0100

    refactor(day23): cleanup

--- src/day23/index.ts
+++ src/day23/index.ts
@@ -16,122 +16,64 @@ const elves = readFileSync(join(__dirname, 'input.txt'), 'utf8')
     return acc;
   }, new Set<string>());
 
-function moveNorth(elves: Set<string>, x: number, y: number): { x: number; y: number } | undefined {
-  const northbound = [
-    { x: x - 1, y: y - 1 },
-    { x, y: y - 1 },
-    { x: x + 1, y: y - 1 },
-  ];
-  if (!northbound.some(({ x, y }) => elves.has(`${x},${y}`))) {
-    return { x, y: y - 1 };
-  }
-}
+const DIRECTIONS = [
+  [
+    { x: -1, y: -1 },
+    { x: +0, y: -1 },
+    { x: +1, y: -1 },
+  ],
+  [
+    { x: -1, y: +1 },
+    { x: +0, y: +1 },
+    { x: +1, y: +1 },
+  ],
+  [
+    { x: -1, y: -1 },
+    { x: -1, y: +0 },
+    { x: -1, y: +1 },
+  ],
+  [
+    { x: +1, y: -1 },
+    { x: +1, y: +0 },
+    { x: +1, y: +1 },
+  ],
+];
 
-function moveSouth(elves: Set<string>, x: number, y: number): { x: number; y: number } | undefined {
-  const soundbound = [
-    { x: x - 1, y: y + 1 },
-    { x: x, y: y + 1 },
-    { x: x + 1, y: y + 1 },
-  ];
-  if (!soundbound.some(({ x, y }) => elves.has(`${x},${y}`))) {
-    return { x, y: y + 1 };
-  }
-}
-
-function moveWest(elves: Set<string>, x: number, y: number): { x: number; y: number } | undefined {
-  const westbound = [
-    { x: x - 1, y: y - 1 },
-    { x: x - 1, y: y },
-    { x: x - 1, y: y + 1 },
-  ];
-  if (!westbound.some(({ x, y }) => elves.has(`${x},${y}`))) {
-    return { x: x - 1, y };
-  }
-}
-
-function moveEast(elves: Set<string>, x: number, y: number): { x: number; y: number } | undefined {
-  const eastbound = [
-    { x: x + 1, y: y - 1 },
-    { x: x + 1, y: y },
-    { x: x + 1, y: y + 1 },
-  ];
-  if (!eastbound.some(({ x, y }) => elves.has(`${x},${y}`))) {
-    return { x: x + 1, y };
-  }
-}
-
-function moveTo(elf: string, turn: number) {
+function moveTo(elf: string, turn: number): string {
   const [x, y] = elf.split(',').map(Number);
-
-  const adjacentPositions = [
-    // Northbound
-    { x: x - 1, y: y - 1 },
-    { x, y: y - 1 },
-    { x: x + 1, y: y - 1 },
-    // Eastbound
-    { x: x + 1, y },
-    // Southbound
-    { x: x - 1, y: y + 1 },
-    { x: x, y: y + 1 },
-    { x: x + 1, y: y + 1 },
-    // Westbound
-    { x: x - 1, y },
-  ];
-
-  const elfNearby = adjacentPositions.some(({ x, y }) => elves.has(`${x},${y}`));
-  if (!elfNearby) {
+  if (DIRECTIONS.every((direction) => direction.every(({ x: dx, y: dy }) => !elves.has(`${x + dx},${y + dy}`)))) {
     return `${x},${y}`; // stay in the same spot
   }
-
-  const directions = [moveNorth, moveSouth, moveWest, moveEast];
-  const shift = turn % 4;
-  if (shift > 0) {
-    directions.splice(directions.length - 1, 0, ...directions.splice(0, shift));
-  }
-  let moveTo: { x: number; y: number } | undefined;
-
-  for (const direction of directions) {
-    moveTo = direction(elves, x, y);
-    if (moveTo) {
-      break;
-    }
-  }
-
-  if (!moveTo) {
-    return `${x},${y}`; // stay in the same spot
-  }
-
-  return `${moveTo.x},${moveTo.y}`;
+  const directions = [...DIRECTIONS];
+  directions.splice(directions.length - 1, 0, ...directions.splice(0, turn % 4));
+  const direction = directions.find((direction) =>
+    direction.every(({ x: dx, y: dy }) => !elves.has(`${x + dx},${y + dy}`)),
+  );
+  return direction ? `${x + direction[1].x},${y + direction[1].y}` : `${x},${y}`;
 }
 
 function move(elves: Set<string>, round: number): number {
   const movements = new Map<string, string[]>();
   let moves = 0;
-
   for (const elf of elves) {
-    // Where can we move the elf to?
     let next = moveTo(elf, round);
     if (next === elf) {
       continue;
     }
-
+    moves += 1;
     const elves = movements.get(next);
     if (!elves) {
       movements.set(next, [elf]);
     } else {
       elves.push(elf);
     }
-
-    moves += 1;
   }
-
   for (const [key, value] of movements) {
     if (value.length === 1) {
       elves.delete(value[0]);
       elves.add(key);
     }
   }
-
   return moves;
 }
 
@@ -170,5 +112,4 @@ do {
 } while (moves !== 0);
 
 console.log(`No elves moved in round ${round}.`);
-
 console.log(getProcessingTime(hrtime(startTime)));
