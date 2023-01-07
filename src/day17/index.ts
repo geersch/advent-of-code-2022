a1acd59eac7d07cef98f286f5540795f1a5f26a0
Christophe Geers <geersch@gmail.com>
2023-01-07 13:06:40 +0100

    day 17 - part 2 (naive solution which works for my puzzle input)

--- src/day17/index.ts
+++ src/day17/index.ts
@@ -8,7 +8,7 @@ const startTime = hrtime();
 const jets = readFileSync(join(__dirname, 'input.txt'), 'utf8').split('');
 
 /**
- * Shape 1   Shape 2   Shape 3   Shape 4   Shape 4
+ * Shape 1   Shape 2   Shape 3   Shape 4   Shape 5
  * --------  --------  --------  --------  --------
  * 00000000  00000000  00000000  00010000  00000000
  * 00000000  00001000  00000100  00010000  00000000
@@ -16,14 +16,6 @@ const jets = readFileSync(join(__dirname, 'input.txt'), 'utf8').split('');
  * 00011110  00001000  00011100  00010000  00011000
  */
 const rocks = [0x1e, 0x81c08, 0x4041c, 0x10101010, 0x1818];
-
-function drawTower(tower: number[]): void {
-  for (let i = tower.length - 1; i >= 0; i--) {
-    const line = tower[i].toString(2).padStart(7, '0').replace(/0/g, '.').replace(/1/g, '#');
-    console.log(`|${line}|`);
-  }
-  console.log('+-------+');
-}
 
 function toBytesInt32(value: number): number[] {
   const buffer = new ArrayBuffer(4);
@@ -49,52 +41,89 @@ function calcTowerMask(tower: number[], height: number): number {
   return mask;
 }
 
-const tower: number[] = [];
-let jetIndex = 0;
-let rockIndex = 0;
-for (let i = 0; i < 2022; i++) {
-  let height = tower.length + 3;
-  let rock = rocks[rockIndex];
-  rockIndex += 1;
-  if (rockIndex % rocks.length === 0) {
-    rockIndex = 0;
+function solve(numberOfRocks: number): { height: number; pattern?: { rocks: number; height: number } } {
+  const seen = new Map<string, { key: string; count: number }>();
+
+  let previous = 0;
+  let rounds = 0;
+
+  const tower: number[] = [];
+  let jetIndex = 0;
+  let rockIndex = 0;
+  for (let i = 0; i < numberOfRocks; i++) {
+    let height = tower.length + 3;
+    let rock = rocks[rockIndex];
+    rockIndex += 1;
+    if (rockIndex % rocks.length === 0) {
+      rockIndex = 0;
+    }
+
+    let atRest = false;
+    while (!atRest) {
+      const jet = jets[jetIndex];
+      jetIndex += 1;
+      if (jetIndex % jets.length === 0) {
+        jetIndex = 0;
+        const key = `${tower.length - previous}_${i - rounds}`;
+        rounds = i;
+        previous = tower.length;
+        const value = seen.get(key);
+        if (!value) {
+          seen.set(key, { key, count: 1 });
+        } else {
+          value.count += 1;
+        }
+      }
+
+      let newPosition = rock;
+      if (jet === '<' && (newPosition & 0x40404040) === 0) {
+        newPosition = newPosition << 1;
+      } else if (jet === '>' && (newPosition & 0x01010101) === 0) {
+        newPosition = newPosition >> 1;
+      }
+      if ((newPosition & calcTowerMask(tower, height)) === 0) {
+        rock = newPosition;
+      }
+
+      atRest = height <= tower.length && (height === 0 || (rock & calcTowerMask(tower, height - 1)) !== 0);
+      if (atRest) {
+        const bytes = toBytesInt32(rock).filter((b) => b !== 0);
+        for (let i = 0; i < bytes.length; i++) {
+          const byte = bytes[i];
+          if (height < tower.length) {
+            tower[height] |= byte;
+          } else {
+            tower.push(byte);
+          }
+          height += 1;
+        }
+      } else {
+        height -= 1;
+      }
+    }
   }
 
-  let atRest = false;
-  while (!atRest) {
-    const jet = jets[jetIndex];
-    jetIndex += 1;
-    if (jetIndex % jets.length === 0) {
-      jetIndex = 0;
-    }
-
-    let newPosition = rock;
-    if (jet === '<' && (newPosition & 0x40404040) === 0) {
-      newPosition = newPosition << 1;
-    } else if (jet === '>' && (newPosition & 0x01010101) === 0) {
-      newPosition = newPosition >> 1;
-    }
-    if ((newPosition & calcTowerMask(tower, height)) === 0) {
-      rock = newPosition;
-    }
-
-    atRest = height <= tower.length && (height === 0 || (rock & calcTowerMask(tower, height - 1)) !== 0);
-    if (atRest) {
-      const bytes = toBytesInt32(rock).filter((b) => b !== 0);
-      for (let i = 0; i < bytes.length; i++) {
-        const byte = bytes[i];
-        if (height < tower.length) {
-          tower[height] |= byte;
-        } else {
-          tower.push(byte);
-        }
-        height += 1;
-      }
-    } else {
-      height -= 1;
-    }
+  if (seen.size > 0) {
+    const parts = [...seen.values()]
+      .sort((a, b) => b.count - a.count)[0]
+      .key.split('_')
+      .map(Number);
+    return { height: tower.length, pattern: { rocks: parts[1], height: parts[0] } };
+  } else {
+    return { height: tower.length };
   }
 }
 
-console.log(`After 2022 rocks have stopped falling the tower is ${tower.length} units tall.`);
+const { height: height1 } = solve(2022);
+console.log(`After 2022 rocks have stopped falling the tower is ${height1} units tall.`);
+
+const { pattern } = solve(10000);
+if (!pattern) {
+  throw new Error('No pattern detected, more iterations required.');
+}
+
+const remainder = solve(1000000000000 % pattern.rocks).height;
+const height2 = Math.floor(1000000000000 / pattern.rocks) * pattern.height + remainder;
+
+console.log(`After 1000000000000 rocks have stopped falling the tower is ${height2} units tall.`);
 console.log(getProcessingTime(hrtime(startTime)));
